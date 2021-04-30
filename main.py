d246c57d4e655299ede6c05022a2aa36007d1b6d
Everton Dutra <evertondutra02@gmail.com>
2021-04-30 18:28:32 -0300

    Criado comando para abrir o navegador

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+navegador = webdriver.Chrome()  # cria um comando para abrir o navegador
+# 1-> Entrar em https://ri.magazineluiza.com.br/default.aspx
+
+# 2-> Cicar em planilha dinâmica
+
+# 3-> Clicar em "clique aqui para fazer o download"
