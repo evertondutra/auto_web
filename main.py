d25e922fb79fae35f2034d86204dc2349c0a608b
Everton Dutra <evertondutra02@gmail.com>
2021-04-30 18:31:51 -0300

    comando para ir até um página específica

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 navegador = webdriver.Chrome()  # cria um comando para abrir o navegador
 # 1-> Entrar em https://ri.magazineluiza.com.br/default.aspx
 
+navegador.get("https://ri.magazineluiza.com.br/default.aspx")  #navega até uma página específica
+
 # 2-> Cicar em planilha dinâmica
 
 # 3-> Clicar em "clique aqui para fazer o download"
